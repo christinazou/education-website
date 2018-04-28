258d7de84e8c1f5c8fc3a14a97412967e3da9793
christinazou <christinazou@users.noreply.github.com>
2018-04-27 23:33:36 -0700

    add new articles for downloading

--- js/main.js
+++ js/main.js
@@ -14,8 +14,24 @@ function myFunction() {
 
 //-------------  slideshow --------------------------------
 var slideIndex = 1;
-showSlides(slideIndex);
 
+
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > x.length - 1) {slideIndex = 1} 
+    x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 5000); // Change image every 2 seconds
+}
+
+
+//showSlides(slideIndex);
 // Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -43,19 +59,8 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
-carousel();
 
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none"; 
-    }
-    slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1} 
-    x[slideIndex-1].style.display = "block"; 
-    setTimeout(carousel, 9000); // Change image every 2 seconds
-}
+
 
 //-------------  slideshow  end --------------------------------
 
